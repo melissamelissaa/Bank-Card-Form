52f0c2a1627d2b7f49ee5a24de0bb502b8ed0ccf
melissamelissaa <ninichiaberashvili2002@gmail.com>
2023-04-01 00:54:50 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import cardFront from "./images/bg-card-front.png";
+import cardBack from "./images/bg-card-back.png";
+// import mainDesktop from "./images/bg-main-desktop.png";
+import { useState } from "react";
+
+const CardPreview = () => {
+  return (
+    <div className="card-images">
+      <img src={cardFront} alt="card Front" />
+      <img src={cardBack} alt="card Back" />
+      {/* <img src={mainDesktop} alt="background-image" /> */}
+    </div>
+  );
+};
+const InputComponent = (props) => {
+  return (
+    <div className="inputComponentContainer">
+      <label htmlFor={props.htmlFor}>
+        {props.labelText}
+      </label>
+      <input
+        className={props.className}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={(e) => props.stateChanger(e.target.value)}
+      ></input>
+    </div>
+  );
+};
+
+const FormContainer = (props) => {
+  return (
+    <form className="form">
+      <InputComponent
+        htmlFor="cardholderName"
+        type="text"
+        placeholder="e.g. Jane Appleseed"
+        labelText="Cardholder Name"
+        required
+        stateChanger={props.setCardholderName}
+      
+      />
+      <InputComponent
+        htmlFor="cardNumber"
+        type="number"
+        placeholder="e.g. 123 45678 9123 0000"
+        labelText="Card Number"
+        required
+        stateChanger={props.setCardNumberInput}
+        
+      />
+      <div className="dateCvcContainer">
+        <div className="inputComponentContainer">
+          <label htmlFor="Date">
+            Exp. Date (MM/YY)
+          </label>
+          <div className="twodDateInputContainer ">
+            <input
+              type="number"
+              placeholder="MM"
+              required
+            ></input>
+            <input
+              type="number"
+              placeholder="YY"
+              required
+            ></input>
+          </div>
+          
+        </div>
+        <InputComponent
+          htmlFor="CVC"
+          type="number"
+          placeholder="e.g. 123"
+          labelText="CVC"
+          required
+        />
+      </div>
+      <button>Confirm</button>
+    </form>
+  );
+};
 
 function App() {
+  const [cardNumberInput, setCardNumberInput] = useState("000 00000 0000 0000");
+  const [cardholderNameInput, setCardholderName] = useState("");
+  const [dateInput, setDateInput] = useState("");
+  const [CvcInput, setCvcInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardPreview />
+      <FormContainer
+        setCardNumberInput={setCardNumberInput}
+        setCardholderName={setCardholderName}
+      />
     </div>
   );
 }
