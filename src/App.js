27106ede6086cf3a4583be984b85bfb5237e5c57
melissamelissaa <ninichiaberashvili2002@gmail.com>
2023-04-05 01:01:59 +0400

    Add spaces in input

--- src/App.js
+++ src/App.js
@@ -99,74 +99,63 @@ const FormContainer = (props) => {
   );
 };
 
-function App() {
-  const [cardNumberInput, setCardNumberInput] = useState("0000 0000 0000 0000");
-  const [cardholderNameInput, setCardholderName] = useState("JANE APPLESEED");
-  const [monthInput, setMonthInput] = useState("00");
-  const [yearInput, setYearInput] = useState("00");
-  const [CvcInput, setCvcInput] = useState("000");
-  const [splitedCardNumber, setSplitedCardNumber] = useState(
-    "0000 0000 0000 0000"
-  );
 
-  // console.log(cardNumberInput);
-  const splitCardNumber = () => {
+  function App() {
+    const [cardNumberInput, setCardNumberInput] = useState("0000000000000000");
+    const [cardholderNameInput, setCardholderName] = useState("JANE APPLESEED");
+    const [monthInput, setMonthInput] = useState("00");
+    const [yearInput, setYearInput] = useState("00");
+    const [CvcInput, setCvcInput] = useState("000");
+   
+    let str = "";
     for (let i = 0; i < cardNumberInput.length; i++) {
-      if (i % 4 == 0) setSplitedCardNumber(splitedCardNumber + " ");
-      if (splitedCardNumber == "0000 0000 0000 0000")
-        setSplitedCardNumber(cardNumberInput[i]);
-      else setSplitedCardNumber(splitedCardNumber + cardNumberInput[i]);
+     if (i % 4 == 0) str += " ";
+   
+     str += cardNumberInput[i];
     }
-  };    
-
-  console.log(splitedCardNumber);
-
-  const inputHandler = (e, maxLength = 50, stateChanger) => {
-    if (e.target.value.length > maxLength) {
+   
+    const inputHandler = (e, maxLength = 50, stateChanger) => {
+     if (e.target.value.length > maxLength) {
       let inputTextToSet = "";
-
+   
       for (let i = 0; i < maxLength; i++) {
-        inputTextToSet += e.target.value[i];
+       inputTextToSet += e.target.value[i];
       }
-
+   
       e.target.value = inputTextToSet;
-    } else {
+     } else {
       stateChanger(e.target.value);
-    }
-  };
-
-  const cardholderNameInputHandler = (e) =>
-    inputHandler(e, 25, setCardholderName);
-  const cardNumberInputHandler = (e) => {
-    inputHandler(e, 16, setCardNumberInput);
-    // splitCardNumber();
-  };
-  const monthInputHandler = (e) => inputHandler(e, 2, setMonthInput);
-  const yearInputHandler = (e) => inputHandler(e, 2, setYearInput);
-  const cvcInputHandler = (e) => inputHandler(e, 3, setCvcInput);
-
-  return (
-    <div className="App">
+     }
+    };
+   
+    const cardholderNameInputHandler = e =>
+     inputHandler(e, 25, setCardholderName);
+    const cardNumberInputHandler = e => inputHandler(e, 16, setCardNumberInput);
+    const monthInputHandler = e => inputHandler(e, 2, setMonthInput);
+    const yearInputHandler = e => inputHandler(e, 2, setYearInput);
+    const cvcInputHandler = e => inputHandler(e, 3, setCvcInput);
+   
+    return (
+     <div className="App">
       <div className="div-in-app">
-        <CardPreview
-          cardNumberInput={splitedCardNumber}
-          cardholderNameInput={cardholderNameInput}
-          monthInput={monthInput}
-          yearInput={yearInput}
-          CvcInput={CvcInput}
-        />
-
-        <FormContainer
-          setCardNumberInput={cardNumberInputHandler}
-          number={cardNumberInput}
-          setCardholderName={cardholderNameInputHandler}
-          setMonthInput={monthInputHandler}
-          setYearInput={yearInputHandler}
-          setCvcInput={cvcInputHandler}
-        />
+       <CardPreview
+        cardNumberInput={str}
+        cardholderNameInput={cardholderNameInput}
+        monthInput={monthInput}
+        yearInput={yearInput}
+        CvcInput={CvcInput}
+       />
+   
+       <FormContainer
+        setCardNumberInput={cardNumberInputHandler}
+        number={cardNumberInput}
+        setCardholderName={cardholderNameInputHandler}
+        setMonthInput={monthInputHandler}
+        setYearInput={yearInputHandler}
+        setCvcInput={cvcInputHandler}
+       />
       </div>
-    </div>
-  );
-}
-
-export default App;
+     </div>
+    );
+   }
+   export default App;
